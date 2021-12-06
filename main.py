efc491969ac87476863553764d310a1d06f8b0bb
Emre <kosenemre@gmail.com>
2021-12-06 11:26:13 +0300

    Add probabilty calculations

--- main.py
+++ main.py
@@ -35,22 +35,50 @@ def calculate_probability(x_test, unique_words_dict, count_mails, total_words_di
     vocabulary = count_vectorizer.get_feature_names_out()
     matrix = matrix.toarray()
 
-    print(vocabulary)
-    print(matrix)
-
     prob_spam = count_mails[0] / (count_mails[0] + count_mails[1])
     prob_ham = count_mails[1] / (count_mails[0] + count_mails[1])
     
+    predictions = []
+    results = {}
+
+    # Iterate over all test samples
     for i in range(x_test.shape[0]):
         probability_spam = math.log(prob_spam)
         probability_ham = math.log(prob_ham)
-        
-        print(len(x_test[i][0]))
 
-        # for j in range():
-        #     cur_word =
-        #     logp = math.log(2)
+        # Iterate over all words of one test sample
+        for j in range(len(matrix[i])):
 
+            if(matrix[i][j] != 0):
+
+                # Spam and ham count starts from 1 because of laplace smoothing
+                spam_count = 1
+                ham_count = 1
+                
+                # We add number of unique words for laplace smoothing
+                spam_denominator = total_words_dist[0] + len(unique_words_dict)
+                ham_denominator = total_words_dist[1] + len(unique_words_dict)
+                
+                word = unique_words_dict.get(vocabulary[j])
+                
+                # Check whether word is in training samples or not
+                if(word is not None):
+                    spam_count += word[0]
+                    ham_count += word[1]
+
+                probability_spam += math.log(spam_count/spam_denominator)
+                probability_ham += math.log(ham_count/ham_denominator)
+
+        print(probability_spam, probability_ham)
+
+        if(probability_spam > probability_ham):
+            predictions.append(1)
+        else:
+            predictions.append(0)
+
+        results[i] = [x_test[i][1], predictions[i]]
+
+    return results
 
 def vectorizer(x):
     # text_col refers to the first column which has mail texts
@@ -112,6 +140,26 @@ def vectorizer(x):
 
     return unique_words_dict, count_mails, total_words_dist
 
+def calculate_performance(results):
+    th = 0
+    ts = 0
+    fh = 0
+    fs = 0
+    for key,value in results.items():
+        if value[0] == value[1] and value[1] == 0:
+            th+=1
+        elif value[0] == value[1] and value[1] == 1:
+            ts+=1
+        if value[0] != value[1] and value[1] == 0:
+            fh+=1
+        elif value[0] != value[1] and value[1] == 1:
+            fs+=1
+    
+    accuracy = (th + ts) / (th + ts + fh + fs)
+    precision = th / (th + fh)
+    recall = th / (th + fs)
+    f1_score = (2 * recall * precision) / (recall + precision)
+    return accuracy, precision, recall, f1_score
 
 def main(total_words_dis=None):
     # reading data's in the csv file to the numpy array
@@ -127,10 +175,12 @@ def main(total_words_dis=None):
     np.random.shuffle(x)
 
     # split data %80 - %20
-    x_test, x_train = split_data(x.copy()[0:50])
+    x_test, x_train = split_data(x.copy())
     unique_words_dict, count_mails, total_words_dist = vectorizer(x_train)
-    calculate_probability(x_test, unique_words_dict, count_mails, total_words_dis)
-    
+    results = calculate_probability(x_test, unique_words_dict, count_mails, total_words_dist)
+    performance = calculate_performance(results)
+    print(performance)
+        
 
 if __name__ == "__main__":
     main()
